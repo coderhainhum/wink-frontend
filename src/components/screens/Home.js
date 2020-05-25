6f7652980d9c5dd379df5e2f9af7389a579ffcf8
Lakshya Bansal <bansallakshya94128@gmail.com>
2020-05-26 01:48:36 +0530

    fetching conversationList

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -1,11 +1,27 @@
-import React,{useState,useContext} from 'react'
+import React,{useState,useEffect} from 'react'
 import {Link,useHistory} from 'react-router-dom'
 const Home =()=>{
     const history=useHistory()
-    
+    const[data,setData]=useState([])
+    useEffect(()=>{
+        fetch('/conversationList')
+        .then(res=>res.json())
+        .then(result=>{
+            setData(result.result)
+            console.log(result.result)
+        })
+    },[])
+
     return(
-        <div className="mycard">
+        <div>
             <h1>Home</h1>
+            {
+                data.map(item=>{
+                    return(
+                    <div key={item._id}></div>
+                    )
+                })
+            }
         </div>
     )
 }
