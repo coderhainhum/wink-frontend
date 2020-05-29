7a0c2c540d8e78eaf37208c7184aef44f75c9d4d
Lakshya Bansal <bansallakshya94128@gmail.com>
2020-05-29 17:56:50 +0530

    logout button

--- src/components/screens/ConversationListItem/index.js
+++ src/components/screens/ConversationListItem/index.js
@@ -14,16 +14,20 @@ export default function ConversationListItem(props) {
     else if(userId==item.person2._id){
         person=item.person1
     }
-    //console.log("item1",item)
     var lastMessage=item.messages.pop()
     
+
+
     return (
-        <div className="conversation-list-item" onClick={()=>{setConversationId(item._id)}}>
-            <img className="conversation-photo" src={person.photo} alt="conversation" />
-            <div className="conversation-info" >
-                <h1 className="conversation-title">{ person.name }</h1>
-                {/* <p className="conversation-snippet">{ lastMessage }</p> */}
+        <div className="conversation-list-box">
+            <div className="conversation-list-item" onClick={()=>{setConversationId(item._id)}}>
+                <img className="conversation-photo" src={person.photo} alt="conversation" />
+                <div className="conversation-info" >
+                    <h1 className="conversation-title">{ person.name }</h1>
+                    {/* <p className="conversation-snippet">{ lastMessage }</p> */}
+                </div>
             </div>
+            
         </div>
     );
 }
--- src/components/screens/Toolbar/index.js
+++ src/components/screens/Toolbar/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './Toolbar.css';
-
+import M from 'materialize-css'
 export default function Toolbar(props) {
     const { title } = props;
+    const logout=()=>{
+      localStorage.clear();
+      window.location.reload();
+      M.toast({html:"logged out successfully",classes:"#43a047 green darken-1"})
+    }
     return (
       <div className="toolbar">
-        <div className="left-items"><i className="material-icons toolbar-button">settings</i></div>
+        <div className="left-items"><i className="material-icons toolbar-button" onClick={()=>logout()}>logout</i></div>
         <h1 className="toolbar-title">{ title }</h1>
         <div className="right-items"><i className="material-icons toolbar-button">add</i></div>
       </div>
